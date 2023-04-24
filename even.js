c3ab80cc28f895fd99f6be641ab29eba14d270e9
Viktor <v.bolotayev@gmail.com>
2023-04-24 03:12:37 +0000

    isEven function

---
+++ even.js
@@ -0,0 +1,11 @@
+> const isEven = function (num) {
+  ...   return num % 2 === 0;
+  ... }
+undefined
+  > 
+  > console.log(isEven(10));
+true
+undefined
+  > console.log(isEven(11));
+false
+undefined
